8d4b5292d12f6a2ce20e332334c248d41c19ec66
umayanga <umayangag@gmail.com>
2022-03-18 01:39:43 +0530

    allow CORS

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 from deep_translator import GoogleTranslator
+from flask_cors import CORS
 import re
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/translate', methods=['POST'])
